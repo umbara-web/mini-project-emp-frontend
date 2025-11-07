8e9a93f3f40eaf4a1226ef201f5708d2e7b0aec2
umbara-web <umbara81.web@gmail.com>
2025-11-08 00:46:02 +0700

    install package/plugin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,16 @@
+import clsx from 'clsx';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 import './globals.css';
 
 import AuthSessionProvider from '../providers/authSessionProvider';
 import AuthWatcher from '../components/authWatcher';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const poppins = Poppins({
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800'],
+  style: ['normal', 'italic'],
+  variable: '--font-poppins',
 });
 
 export const metadata: Metadata = {
@@ -27,9 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={clsx(poppins.variable, 'antialiased')}>
         <AuthSessionProvider>
           {children}
           <AuthWatcher />
